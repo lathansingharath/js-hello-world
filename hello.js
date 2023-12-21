281b4474cd03c624212fa6007a43798ad3ea9796
Lathansingharath <singharath@hotmail.com>
2023-12-20 22:13:35 -0600

    Changed message

--- hello.js
+++ hello.js
@@ -4,7 +4,9 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello Lance Taylor Hodge");
+console.log("My favorite game is 'Halo'");
+
+
 
 
 
